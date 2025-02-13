326a2120ac7484c9c8b23196adfb2c6c0446ecba
Pranav <khandagalepranav1546@gmail.com>
2025-02-14 00:17:01 +0530

    chat

--- backend/chat/chat.js
+++ backend/chat/chat.js
@@ -1,26 +1,45 @@
-const {WebSocketServer} = require('ws');
+const { WebSocketServer } = require("ws");
+const jwt = require('jsonwebtoken');
+const dotenv = require("dotenv");
+
+dotenv.config();
+const JWT_SECRET = process.env.JWT_SECRET;
 
 
 function setUpWebsocket(server) {
     const wss = new WebSocketServer({ server });
-    
-    wss.on("connection",function (socket) {
-        console.log("New Websocket Connection");
-        
-        socket.on("message", function (message) {
-            console.log("Received msg: "+message.toString());
-            socket.send(JSON.stringify({
-                message: message.toString()
-            }));
-        })
-        socket.send("Websocket connected");
-    })
-    
-    console.log("Websocket server started n port 3000");
+
+    wss.on("connection", (socket, req) => {
+        const token = req.headers["sec-websocket-protocol"];
+        if (!token) {
+            socket.close(400, "Authentication Required");
+            return;
+        }
+        try {
+            const data = jwt.verify(token, JWT_SECRET);
+            socket.username = data.username;
+            console.log("User connected: ", socket.username);
+            socket.on("message", (message) => {
+                console.log("Received msg: " + message.toString());
+                const msg = JSON.parse(message);
+
+                socket.send(JSON.stringify({
+                    text: msg.text,
+                    user: "Server"
+                }));
+            });
+
+            socket.send("WebSocket connected");
+        } catch (e) {
+            console.log("Invalid token: ", e.message);
+            socket.close(400, "Invalid token")
+
+        }
+
+    });
+
+    console.log("WebSocket server started on port 3000");
     return wss;
 }
 
-
-module.exports = {
-    setUpWebsocket
-}
+module.exports = { setUpWebsocket };
