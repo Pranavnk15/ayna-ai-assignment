4920afe9f1430d805ec2ab1bce6873292e1b1b02
Pranav <khandagalepranav1546@gmail.com>
2025-02-14 11:32:20 +0530

    updated url

--- frontend/src/pages/chat.jsx
+++ frontend/src/pages/chat.jsx
@@ -42,7 +42,7 @@ export default function ChatApp() {
             alert("You must be logged in to chat");
             navigate("/signin");
         }
-        socketRef.current = new WebSocket(`wss://chat-app-backend-two-lime.vercel.app/?token=${token}`);
+        socketRef.current = new WebSocket(`ws://chat-websocket-server-u878.onrender.com?token=${token}`);
 
         socketRef.current.onopen = () => setWsStatus("Connected");
         socketRef.current.onclose = () => setWsStatus("Closed");
